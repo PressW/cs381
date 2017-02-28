04f2694c3d7187ebac02b40db1f7f2f1b21ed7f7
Preston Wipf <gullysquad02@gmail.com>
2017-02-27 18:09:55 -0800

    Adding skeleton for stmt valuation function

--- hw4/KarelSemantics.hs
+++ hw4/KarelSemantics.hs
@@ -15,14 +15,26 @@ test (Clear dir)   w r = isClear (relativePos dir r) w
 test (Beeper)      w r = hasBeeper (getPos r) w
 test (Empty)       _ r = isEmpty r
 
+
+
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown   _ _ r = Done r
+stmt Move       _ _ _ = undefined
 stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
-stmt _ _ _ _ = undefined
+stmt PutBeeper  _ _ _ = undefined
+stmt Turn       _ _ _ = undefined
+stmt Block      _ _ _ = undefined
+stmt Block      _ _ _ = undefined
+stmt If         _ _ _ = undefined
+stmt Call       _ _ _ = undefined
+stmt Iterate    _ _ _ = undefined
+stmt While      _ _ _ = undefined
+    
+    
     
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
